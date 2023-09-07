2cb977d606087fce84dee48c96880c1500e851cb
Norrick McGee <norrick.mcgee@gmail.com>
2023-09-06 19:26:08 -0500

    Example potion table data added

--- table-forge.py
+++ table-forge.py
@@ -18,19 +18,39 @@ class MainGrid(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.add_widget(HomeScreen())
-        rollscreen1 = RollScreen()
-        rollscreen2 = RollScreen()
+        rollscreen1 = RollScreen('random-potions')
+        rollscreen2 = RollScreen('loot-drow')
 
 class RollTable(pandas.DataFrame):
-    def __init__(self, **kwargs):
+    def __init__(self, description, **kwargs):
         super().__init__(**kwargs)
-
+        self.description = description
 
 
 class RollScreen(Screen):
-    def __init__(self, **kwargs):
+    def __init__(self, _dev_rollscreen_str, **kwargs):
+        '''
+        _dev_rollscreen_str: 
+            string for initializing a RollTable for a RollScreen
+        '''
         super().__init__( **kwargs)
-        rolltable1 = RollTable()
+        
+
+
+        ### Dev -- Generating RandomTable ###
+        if _dev_rollscreen_str == 'random-potions':
+            table = {
+                      'roll': [1, 2],
+                      'potion': ['healing potion', 'mana potion'],
+                      'description': ['heals 2d10 damage', 'restores 1d8 mana']
+                    } 
+                    
+            self.roll_table = RollTable('Random Potions')
+        elif _dev_rollscreen_str == '':
+            self.roll_table = RollTable('Drow Loot')
+        else:
+            self.roll_table = RollTable('A New Roll Table')
+
 
 class HomeScreen(Screen):
     def __init__(self, **kwargs):
